1c654c52d4527b7ad0b69523d5fb099be72f1d42
Hanna <s2010456010@fhooe.at>
2023-05-24 14:57:28 +0200

    24.05

--- database/migrations/2023_05_13_090217_create_comments_table.php
+++ database/migrations/2023_05_13_090217_create_comments_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('entrie_id')->constrained()->onDelete('cascade');
-            $table->text('comment');
+            $table->text('comment')t;
             $table->timestamps();
         });
     }
